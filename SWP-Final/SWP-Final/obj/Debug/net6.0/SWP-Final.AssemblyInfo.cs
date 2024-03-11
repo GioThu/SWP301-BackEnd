90c87167f1dec7a1d25f12bfba7113c649687319
anh231102 <anh231102@gmail.com>
2024-03-11 15:24:19 +0700

    UpdateApartmentTool

--- SWP-Final/SWP-Final/obj/Debug/net6.0/SWP-Final.AssemblyInfo.cs
+++ SWP-Final/SWP-Final/obj/Debug/net6.0/SWP-Final.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP-Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed708dcd7626cde20a6b134d18e152fde2ea9583")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP-Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP-Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
